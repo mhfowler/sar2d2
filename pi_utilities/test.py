466eec007ad53e54aa49aaef8f9a545e6d264e97
Max Fowler <maxhfowler@gmail.com>
2020-01-29 22:01:22 +0100

    update to python3

--- pi_utilities/test.py
+++ pi_utilities/test.py
@@ -3,7 +3,7 @@ import os
 
 if __name__ == '__main__':
     log_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '../../log'))
-    print log_dir
+    print(log_dir)
     out_file_path = os.path.join(log_dir, 'simple_python.log')
     with open(out_file_path, 'w') as out_file:
         out_file.write(out_file_path)
