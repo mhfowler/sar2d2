2212e0ce72afbc2311b5d7b6d1af250dbb91276e
Max Fowler <maxhfowler@gmail.com>
2020-02-07 15:05:35 +0100

    log

--- js/ngrok_tcp_connect.js
+++ js/ngrok_tcp_connect.js
@@ -2,9 +2,14 @@ var ngrok = require('ngrok');
 var util = require('util');
 var secrets = require('../ansible-pi/secret_files/secret.json');
 
+
+function _log(msg) {
+	console.log(msg)
+}
+
 // log console messages
 var console_msg = util.format('++ attempting to connect with ngrok');
-send_telegram_message(console_msg);
+_log(console_msg);
 
 ngrok.connect({
 	proto: 'tcp', // http|tcp|tls
@@ -14,13 +19,13 @@ ngrok.connect({
 	//subdomain: 'alex', // reserved tunnel name https://alex.ngrok.io,
 	//authtoken: '12345' // your authtoken from ngrok.com
 }, function (err, url) {
-	send_telegram_message('++ ngrok connected');
-	send_telegram_message(util.format('++ error: %j', err));
-	send_telegram_message(util.format('++ url: %s', url));
+	_log('++ ngrok connected');
+	_log(util.format('++ error: %j', err));
+	_log(util.format('++ url: %s', url));
 	// tcp://0.tcp.ngrok.io:12747
 	var myRegexp = /tcp:\/\/(\d+\.tcp\.ngrok\.io)\:(\d+?)$/g;
 	var match = myRegexp.exec(url);
 	var cmd_str = util.format('ssh pi@%s -p%s', match[1], match[2]);
-	send_telegram_message('++ ' + cmd_str);
+	_log('++ ' + cmd_str);
 	twitter_helper.post_tweet(cmd_str);
 });
