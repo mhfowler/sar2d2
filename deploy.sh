aa42e7f07036b1ca34b4c58880c6657993e94fa9
Max Fowler <maxhfowler@gmail.com>
2020-04-03 22:28:04 +0200

    update body

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 #ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass -c paramiko
-ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass -c paramiko
+ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass -c paramiko --tags code
 #ansible -i ansible-pi/hosts pis -m ping --ask-pass -c paramiko
 #ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass --sudo
 #ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass --sudo --tags code
