57281985a1d36365e9ef8e4f502e4737c7c6ce75
Max Fowler <maxhfowler@gmail.com>
2020-04-03 21:31:44 +0200

    multiple records updated

--- ansible-pi/roles/pi/templates/cron/dynamicdns.sh
+++ ansible-pi/roles/pi/templates/cron/dynamicdns.sh
@@ -1,3 +1,6 @@
-source {{bash_profile}}
+. {{bash_profile}}
 
-$PYTHON {{src_dir}}/python_utilities/update_dns.py  > {{log_dir}}/dynamicdns.log 2> {{log_dir}}/dynamicdns.error &
+now=$(date)
+echo "running dynamicdns.sh" > {{log_dir}}/dynamicdns.log
+echo $now >> {{log_dir}}/dynamicdns.log
+$PYTHON {{src_dir}}/pi_utilities/update_dns.py  >> {{log_dir}}/dynamicdns.log 2> {{log_dir}}/dynamicdns.error &
--- pi_utilities/update_dns.py
+++ pi_utilities/update_dns.py
@@ -7,19 +7,21 @@ from hello_settings import SECRETS_DICT, PROJECT_PATH
 def update_dns():
     ACCOUNT_ID = SECRETS_DICT['DNSSIMPLE_ID']
     TOKEN = SECRETS_DICT['DNSSIMPLE_TOKEN']
-    RECORD_ID = SECRETS_DICT['DNSSIMPLE_RECORD_ID']
-    ZONE_ID = SECRETS_DICT['DNSSIMPLE_ZONE_ID']
-    telegram_log('++ updating dns for {}'.format(ZONE_ID))
+    RECORDS = SECRETS_DICT['DNSSIMPLE_RECORDS']
 
     script_path = os.path.join(PROJECT_PATH, 'bash/update_dns.sh')
-    bash_cmd = 'ACCOUNT_ID={ACCOUNT_ID} TOKEN={TOKEN} RECORD_ID={RECORD_ID} ZONE_ID={ZONE_ID} {script_path}'.format(
-        ACCOUNT_ID=ACCOUNT_ID,
-        TOKEN=TOKEN,
-        RECORD_ID=RECORD_ID,
-        ZONE_ID=ZONE_ID,
-        script_path=script_path
-    )
-    os.system(bash_cmd)
+    for record in RECORDS:
+        RECORD_ID = record['RECORD_ID']
+        ZONE_ID = record['ZONE_ID']
+        telegram_log('++ updating dns for {} {}'.format(ZONE_ID, RECORD_ID))
+        bash_cmd = 'ACCOUNT_ID={ACCOUNT_ID} TOKEN={TOKEN} RECORD_ID={RECORD_ID} ZONE_ID={ZONE_ID} {script_path}'.format(
+            ACCOUNT_ID=ACCOUNT_ID,
+            TOKEN=TOKEN,
+            RECORD_ID=RECORD_ID,
+            ZONE_ID=ZONE_ID,
+            script_path=script_path
+        )
+        os.system(bash_cmd)
 
 
 if __name__ == '__main__':
