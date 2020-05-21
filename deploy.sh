0af1f10e5341becd31f456d560281e7c7497ffd6
Max Fowler <maxhfowler@gmail.com>
2020-05-21 16:52:44 +0200

    reboot_util cron job

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
-ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass -c paramiko --tags code
+ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass -c paramiko
 #ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass -c paramiko --tags letsencrypt
 #ansible -i ansible-pi/hosts pis -m ping --ask-pass -c paramiko
 #ansible-playbook ansible-pi/playbook.yml -i ansible-pi/hosts --ask-pass --sudo
