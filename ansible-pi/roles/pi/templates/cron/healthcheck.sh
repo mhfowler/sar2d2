21fba83b2f765d508e1308022e835d506026f70a
Max Fowler <maxhfowler@gmail.com>
2020-05-27 13:31:40 +0200

    run healthcheck as pi user

--- ansible-pi/roles/pi/templates/cron/healthcheck.sh
+++ ansible-pi/roles/pi/templates/cron/healthcheck.sh
@@ -3,4 +3,5 @@
 now=$(date)
 echo "running healthcheck.sh" > {{log_dir}}/healthcheck.log
 echo $now >> {{log_dir}}/healthcheck.log
+nvm list
 $PYTHON {{src_dir}}/pi_utilities/health_check.py  >> {{log_dir}}/healthcheck.log 2> {{log_dir}}/healthcheck.error &
